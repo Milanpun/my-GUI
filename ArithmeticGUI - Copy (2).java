619586f5d4223633459c75dd385ccda713225168
Milanpun <104912826+Milanpun@users.noreply.github.com>
2022-05-04 20:12:12 +0545

    Add files via upload

---
+++ ArithmeticGUI - Copy (2).java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+//public class ArithmeticGUI {
+
+       // JFrame myframe;
+
+        // constructor
+      //  public first GuI() {
+           //     myframe = new JFrame("my frame");
+          //      myframe.setSize(400, 300);
+           ///     myframe.setVisible(true);
+    //    }
+
+      //  public static void main(String[] args) {
+             //   new firstGUI();
+      //  }
+
+
+     //   }
---
+++ Palindrome - Copy (2).java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//public class Palindrome extends JFrame{
+  //  JLabel lblfirst;
+    //JTextField txtfirst;
+    //JButton btncheck, btnclose;
+
+
+    //public Palindrome(){
+      //  setTitle("check palindrome");
+        //setSize( 400,  200 );
+        //setLayout(null);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setLocationRelativeTo(null);
+        //setVisible(true);
+
+      //Container c = getContentPane();
+   //lblfirst = new JLabel("enter the number");
+     //   lblfirst.setBounds(20,20,150,20);
+       //txtfirst = new JTextField();
+       //txtfirst.setBounds(20, 40, 150,50);
+       //btncheck = new JButton("check palindrome");
+       //btncheck.setBounds(20,60,180,50);
+
+        //btnClose = new JButton("Close");
+        //btnclose.setBounds(20,60,200,50);
+
+
+        //btnCheck.addActionListener(new ActionListener() {
+
+          //  @Override
+            //public void actionPerformed(ActionEvent e) {
+
+            //}
+        //});
+
+      ///  btnClose.addActionListener( new ActionListener() {
+
+          //  @Override
+            //public void actionPerformed(ActionEvent e) {
+
+            //}
+        //});
+
+            //public static void main(String[] args) {
+          //      new Palindrome();
+
+            //}
+
+
